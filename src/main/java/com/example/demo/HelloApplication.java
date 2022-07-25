101255381f11db8565a496d13b347768bb3fdd7c
mh.mehdikhani.2003 <mhms2003bzm@gmail.com>
2022-07-25 17:02:35 +0430

    Second Commit

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -14,7 +14,6 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        System.out.println("nigga");
         stage.show();
     }
 
