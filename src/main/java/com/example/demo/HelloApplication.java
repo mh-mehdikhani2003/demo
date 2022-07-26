6dd79bd539401760ae92d72798edfe8e2c53cd54
mh.mehdikhani.2003 <mhms2003bzm@gmail.com>
2022-07-26 16:30:22 +0430

    third Commit

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,19 +3,32 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    static Stage  MainStage;
+    static int Height = 500;
+    static int Width = 800;
+
     @Override
     public void start(Stage stage) throws IOException {
+        MainStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        System.out.println("hee");
+        Scene scene = new Scene(fxmlLoader.load(), Width,Height );
+        stage.setTitle("OopGram");
         stage.setScene(scene);
+        Image image = new Image("C:\\Users\\mhmeh\\Desktop\\oop project\\demo\\src\\main\\resources\\icons8-instagram-40.png");
+        stage.getIcons().add(image);
+
+        stage.setResizable(false);
         stage.show();
+    }
+
+    public static Stage getMainStage() {
+        return MainStage;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Pane RegisterPane;
+
 
     @FXML
     protected void onHelloButtonClick() {
